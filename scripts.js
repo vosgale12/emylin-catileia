25f5e6523941bc8a8ba87c3f5ba68c2067ecaf8d
vosgale12 <joaovosgale40@gmail.com>
2025-04-22 15:22:43 +0000

    arrumei o contraste

--- scripts.js
+++ scripts.js
@@ -5,10 +5,31 @@ document.addEventListener('DOMContentLoaded', function(){
     let tamanhoDaFonte = 1
     // define o tamanho da fonte na variável
 
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
     botaoAumentarFonte.addEventListener('click', function(){
         tamanhoDaFonte = tamanhoDaFonte + 0.1
         document.body.style.fontSize = `${tamanhoDaFonte}rem`
     })
+    const botaoDiminuir = document.getElementById('diminuir-fonte') 
+    botaoDiminuir.addEventListener('click', function(){
+        tamanhoDaFonte = tamanhoDaFonte - 0.1;
+        document.body.style.fontSize = tamanhoDaFonte+"rem";
+        
+
+    })
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function(){
+            document.body.classList.toggle('alto-contraste')
+    })
+
+    botaoAcessibilidade.addEventlistener('click', function(){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista');
+    })
+  
 
 
 })
+
